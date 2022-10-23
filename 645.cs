15e266061d0041b474e0b514eccdb8f20b4fc5df
Faiyaz Ibrahim <>
2022-10-23 15:29:36 +0600

    init

---
+++ 645.cs
@@ -0,0 +1,30 @@
+﻿public class Solution {
+
+
+    static void Main(string[] args)
+    {
+        int[] arr = { 1, 2, 2, 4};
+        Console.WriteLine(FindErrorNums(arr));
+    }
+
+    public static int[] FindErrorNums(int[] nums) {
+	var arr = new int[nums.Length+1];
+	var d = -1;
+	for(var i = 0; i < nums.Length; ++i){
+		arr[nums[i]]++;
+		// optimization
+		if(arr[nums[i]]>1)
+        {
+            d = nums[i];
+        }
+	}
+	for(var i = 1; i < arr.Length; ++i)
+		//  missing
+		if(arr[i] == 0)
+        {
+            Console.WriteLine(d + "  " + i);
+            return new int[]{d, i};
+        }
+	return new int[2];
+}
+}
