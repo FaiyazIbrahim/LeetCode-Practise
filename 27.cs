6402f339fd613477adff5e57eb9afe45d00ab73b
rootfiz <faiyazibrahim0@gmail.com>
2022-10-25 22:44:26 +0600

    Solved Pproblem 27

--- 27.cs
+++ 27.cs
@@ -1,25 +1,36 @@
-public class Solution27{
+public class Solution27
+{
 
 
     public void Test()
     {
-        int[] nums = {0,1,2,2,3,0,4,2};
-        int val = 2;
-        int result = 0;
-
-        for(int k = 0; k < nums.Length; k++)
-        {
-            if(nums[k] == val)
-            {
-                result++;
-            }
-        }
-        Console.WriteLine(result);
+        //int[] nums = {3,2,2,3};
+        int[] nums = { 0, 1, 2, 2, 3, 0, 4, 2 };
+        Console.WriteLine(RemoveElement(nums, 2));
     }
 
 
-    // public int RemoveElement(int[] nums, int val)
-    // {
+    public int RemoveElement(int[] nums, int val)
+    {
+        int i = -1;
 
-    // }
+        for (int k = 0; k < nums.Length; k++)
+        {
+            if (nums[k] == val)
+            {
+                if (i == -1)
+                {
+                    i = k;
+                }
+
+            }
+            else if (i != -1)
+            {
+                nums[i++] = nums[k];
+            }
+
+        }
+        return i != -1 ? i : nums.Length;
+
+    }
 }
