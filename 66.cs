397195b20569b2e5c7cb64f7bb9c622538e790ce
rootfiz <faiyazibrahim0@gmail.com>
2022-10-30 01:48:25 +0600

    WIP Problem 66

--- 66.cs
+++ 66.cs
@@ -3,19 +3,30 @@ public class Solution66
 
     public void Test()
     {
-        int[] k = {1,2,3};
+        int[] k = {9};
         PlusOne(k);
     }
 
     public int[] PlusOne(int[] digits)
     {
-        int[] arr = new int[]{};
+        List<int> arr = new List<int>();
+        for(int i = 0; i < digits.Length - 1; i++)
+        {
+            arr.Add(digits[i]);
+        }
         int k = digits[^1] + 1;
-   
-        int a = 100;
-        int b = a / 10;
-        int c = a % 10;
-        Console.WriteLine(b + " " + c);
-        return arr;
+        if(k < 9)
+        {
+            arr.Add(k);
+        }
+        else
+        {
+            int a = k;
+            int b = a / 10;
+            int c = a % 10;
+            arr.Add(b);
+            arr.Add(c);
+        }
+        return arr.ToArray();
     }
 }
