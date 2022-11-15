3fcbc2455fbcad6e53e1caa888073f8fcd000ce6
rootfiz <faiyazibrahim0@gmail.com>
2022-11-16 01:08:19 +0600

    WIP 206

---
+++ 206.cs
@@ -0,0 +1,13 @@
+System.Collections.Generic.LinkedList<T>
+public class Solution206
+{
+    public void Test()
+    {
+
+    }
+
+    // public ListNode ReverseList(ListNode head)
+    // {
+
+    // }
+}
